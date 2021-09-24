6e4c3c39c1a2cbc12902a85d04addf6b263c2746
JohnMoran <johnnymoran44@hotmail.com>
2021-09-24 13:12:34 +0200

    Update promises.js

--- promises.js
+++ promises.js
@@ -2,3 +2,13 @@
 await sleepNSec(5)
 console.log(“stop”)  */
 //should be desplayed after 5 sec
+
+async function sleepNSec(time) {
+	return await new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(console.log("stop"));
+		}, time * 5);
+	});
+}
+
+sleepNSec(5);
